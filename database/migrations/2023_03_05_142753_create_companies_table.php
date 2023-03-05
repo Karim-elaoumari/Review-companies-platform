b7c4583a90baa83f8b2a31b8ea1f2e8d3f53da33
ELKABLI <amina.elkabli1@gmail.com>
2023-03-05 16:43:24 +0100

    added the migration

---
+++ database/migrations/2023_03_05_142753_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('logo');
+            $table->string('website');
+            $table->integer('founded');
+            $table->integer('employees');
+            $table->decimal('revenue', 20, 2);
+            $table->longText('description');
+            $table->longText('mission');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
